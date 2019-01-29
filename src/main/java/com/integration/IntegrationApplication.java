cdb7745101b456baf3712b0f6c95b675e3f84f4d
Guilherme Lourenco <guilherme@ampliato.com.br>
2019-01-29 13:45:20 -0200

    Insert User and Book for testing

--- src/main/java/com/integration/IntegrationApplication.java
+++ src/main/java/com/integration/IntegrationApplication.java
@@ -1,10 +1,49 @@
 package com.integration;
 
+import com.integration.book.models.Book;
+import com.integration.book.repositories.BookRepository;
+import com.integration.user.models.User;
+import com.integration.user.repositories.UserRepository;
+
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
+@RestController
 public class IntegrationApplication {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    public void addData2DB() {
+        userRepository.saveAll(Stream.of(new User(744, "John"), new User(455, "Pitter")).collect(Collectors.toList()));
+        bookRepository.saveAll(
+                Stream.of(new Book(111, "Core Java"), new Book(222, "Spring Boot")).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/getUsers")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/getBooks")
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IntegrationApplication.class, args);
